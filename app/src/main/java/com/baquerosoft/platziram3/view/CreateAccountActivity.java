f9338e02a53d8d05b6101fb3784657b28cdcee25
Wilinton Baquero <willys96@gmail.com>
2017-01-14 12:59:15 -0500

    Inclusion del Logo, y los textview de Usuario y password en el activity Login

---
+++ app/src/main/java/com/baquerosoft/platziram3/view/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.baquerosoft.platziram3.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.baquerosoft.platziram3.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
