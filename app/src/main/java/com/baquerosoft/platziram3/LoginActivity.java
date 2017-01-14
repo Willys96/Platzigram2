d4a3b3be532db568a129139fb35836e755b55dc2
Wilinton Baquero <willys96@gmail.com>
2017-01-14 15:18:33 -0500

    Inicio de Edicion de Activity CreateAccount

--- app/src/main/java/com/baquerosoft/platziram3/LoginActivity.java
+++ app/src/main/java/com/baquerosoft/platziram3/LoginActivity.java
@@ -2,6 +2,9 @@ package com.baquerosoft.platziram3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    protected void goCreateAccount(View view){
+        Toast.makeText(getBaseContext(),"Ahora crearás tu cuenta!",Toast.LENGTH_LONG).show();
+
     }
 }
