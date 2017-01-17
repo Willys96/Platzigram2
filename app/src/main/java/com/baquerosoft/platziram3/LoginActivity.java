87ca761d732a2d566be2162ce75c076e3471018c
Wilinton Baquero <willys96@gmail.com>
2017-01-16 19:08:39 -0500

    Adicion de intent para llamar a la Activity CreateAccount y edicion del manifest para indicar la Activity padre de la anterior (Login)

--- app/src/main/java/com/baquerosoft/platziram3/LoginActivity.java
+++ app/src/main/java/com/baquerosoft/platziram3/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.baquerosoft.platziram3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import com.baquerosoft.platziram3.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -16,6 +19,9 @@ public class LoginActivity extends AppCompatActivity {
 
     protected void goCreateAccount(View view){
         Toast.makeText(getBaseContext(),"Ahora crearás tu cuenta!",Toast.LENGTH_LONG).show();
+
+            Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
 
     }
 }
