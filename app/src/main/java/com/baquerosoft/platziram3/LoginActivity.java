6db92fa8a274a275c4b92eb3d088cc3a838a79a9
Wilinton Baquero <willys96@gmail.com>
2017-01-17 21:27:53 -0500

    Personalizacion de bottombar, adicion de este a activity Container y llamado con intent de esta desde activity Login

--- app/src/main/java/com/baquerosoft/platziram3/LoginActivity.java
+++ app/src/main/java/com/baquerosoft/platziram3/LoginActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.baquerosoft.platziram3.view.ContainerActivity;
 import com.baquerosoft.platziram3.view.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -21,6 +22,14 @@ public class LoginActivity extends AppCompatActivity {
         Toast.makeText(getBaseContext(),"Ahora crearás tu cuenta!",Toast.LENGTH_LONG).show();
 
             Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+
+    }
+
+    protected void goContainer(View view){
+        //Toast.makeText(getBaseContext(),"Ahora crearás tu cuenta!",Toast.LENGTH_LONG).show();
+
+        Intent intent = new Intent(this, ContainerActivity.class);
         startActivity(intent);
 
     }
